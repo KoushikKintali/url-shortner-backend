7f03d0a1459acb89b35916d141db4e92b33b110b
KoushikKintali <44541750+KoushikKintali@users.noreply.github.com>
2021-06-13 22:00:17 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors({ origin: 'https://url-shortner-backend-1.netlify.app' });
+  app.enableCors({ origin: 'https://url-shortner-backend-1.netlify.app', optionsSuccessStatus: 200 });
   await app.listen(3000);
 }
 bootstrap();
