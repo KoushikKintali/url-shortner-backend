90a01bc55c52a31d742048101bd64aece6e12a60
KoushikKintali <44541750+KoushikKintali@users.noreply.github.com>
2021-06-13 22:28:09 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
   });
-  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
